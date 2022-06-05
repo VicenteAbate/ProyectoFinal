7f7a6a61b0bd19687ca3518d1ba31b2da423326d
natmacira <natimciraolo@gmail.com>
2022-06-05 18:49:37 -0300

    form para agregar pokemons en el front

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import Buscador from './Buscador';
 import Detalles from './Detalles';
 import Boton from './Boton';
+import { useState } from "react";
 
 function App(props) {
+  const [showThings, setShowThings] = useState(false);
+
+  const showOrHide = () => {
+    setShowThings(!showThings);
+  };
   return (
     <>
-       <Boton />
+       <Boton showImage={showThings} showOrHideImage={showOrHide} />
       <Buscador />
       <Detalles />
      
--- pokedex-app/src/Boton.jsx
+++ pokedex-app/src/Boton.jsx
@@ -1,11 +1,59 @@
 // import { useState } from "react";
 
-const Boton = () => {
 
-    return <div >
-        <button className="nuevo-pokemon-btn">Nuevo Pokemon</button>
-    </div>
-}
 
+// const Boton = () => {
+
+
+//     // const [formNueovPokemon, setFormNueovPokemon] = useState("")
+
+//     function formNueovPokemon() {
+
+
+//         console.log('You clicked Nuevo Pokemon.');
+
+//         return (
+//         <form action="">
+//         <input type="text" />Name
+//         </form>)
+//       }
+
+//     return <div >
+//         <button className="nuevo-pokemon-btn" onClick={formNueovPokemon}>Nuevo Pokemon</button>
+//     </div>
+// }
+
+const Boton = (props) => {
+    return (
+        <div className="treeImageContainer">
+            <button onClick={props.showOrHideImage} className="nuevo-pokemon-btn">
+                Nuevo Pokemon
+            </button>
+            {props.showImage ? (
+                <form action="" className="form-nuevo-pkmn">
+                    <div className="form-div">
+                        <label htmlFor="">Pokemon Name</label><input type="text" className="input-form" required/>
+                    </div>
+                    <div className="form-div">
+                        <label htmlFor="">Pokemon Element1</label><input type="text" className="input-form" required/>
+                    </div>
+                    <div className="form-div">
+                        <label htmlFor="">Pokemon Element2</label><input type="text" className="input-form"/>
+                    </div>
+                    <div className="form-div">
+                        <label htmlFor="">Pokemon ID</label><input type="number" className="input-form" required/>
+                    </div>
+                    <div className="form-div">
+                       <button className="nuevo-pokemon-btn">Enviar</button>
+                    </div>
+                </form>
+            )
+                : (
+                    <span></span>
+                )
+            }
+        </div>
+    );
+};
 
 export default Boton;
