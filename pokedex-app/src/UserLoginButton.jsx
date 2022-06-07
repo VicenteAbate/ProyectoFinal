e1f9f807208137c70220f1af2a30828baaad628f
vicente <vicenteabate.22@gmail.com>
2022-06-07 20:59:42 -0300

    Intento de conexion entre front y back para login

--- pokedex-app/src/UserLoginButton.jsx
+++ pokedex-app/src/UserLoginButton.jsx
@@ -1,8 +1,45 @@
-
-
-
+import { useState } from "react";
 
 const UserLogin = (props) => {
+
+    const [userName, setUserName] = useState()
+
+    const setJwt = async () => {
+        try {
+            const respuesta = await fetch("http://localhost:3000/pkmn/usuarios/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    mail: userName,
+                    password: userPass,
+                })
+            })
+
+            const respuestaJson = await respuesta.json();
+            console.log(respuestaJson)
+            localStorage.setItem("jwt", respuestaJson.token)
+        } catch (error) {
+            console.log(error)
+
+        }
+    };
+    const [userPass, setUserPass] = useState()
+
+    const onClick = (e) => {
+        e.preventDefault();
+        setJwt();
+    }
+
+    const onChangeUserName = (e) => {
+        setUserName(e.target.value);
+    };
+
+    const onChangeUserPass = (e) => {
+        setUserPass(e.target.value);
+
+    };
     return (
         <div className="loginImageContainer">
             <button onClick={props.showOrHideLogin} className="login-button">
@@ -12,14 +49,28 @@ const UserLogin = (props) => {
                 <form action="" className="form-login">
                     <div className="login-form-sections">
                         <span>Welcome to</span>
-                        <span> <br/> </span>
+                        <span> <br /> </span>
                         <span>Pokedex</span>
                         <div> <br /> </div>
                         <div className="form-div-login">
-                            <label htmlFor="">Email</label><input type="text" className="input-form" required />
+                            <label htmlFor="">Email</label><input
+                                value={userName}
+                                id="pkmn"
+                                type="text"
+                                name="pkmn"
+                                placeholder="Username or Email"
+                                className="input-form"
+                                onChange={onChangeUserName} required />
                         </div>
                         <div className="form-div-login">
-                            <label htmlFor="">Password</label><input type="password" className="input-form" required />
+                            <label htmlFor="">Password</label><input
+                                value={userPass}
+                                id="pkmn"
+                                type="password"
+                                name="pkmn"
+                                placeholder="Password"
+                                className="input-form"
+                                onChange={onChangeUserPass} required />
                         </div>
                         <div className="form-div-login">
                             <button className="login-button">Enviar</button>
